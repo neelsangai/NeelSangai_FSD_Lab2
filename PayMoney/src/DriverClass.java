70088f7061cc662df90d726e70c620f63d735182
neelsangai <113979801+neelsangai@users.noreply.github.com>
2022-10-11 23:34:10 +0530

    Add files via upload

---
+++ PayMoney/src/DriverClass.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class DriverClass {
+	public static void main(String[] args) {
+		
+	Scanner sc=new Scanner(System.in);					 
+	System.out.println("Enter No of transaction");	
+	int size=sc.nextInt();
+	
+	int trans[]=new int[size];
+	for(int i=0;i<size;i++)
+	{
+		trans[i]=sc.nextInt();
+	}
+
+	System.out.println("Enter No of test cases");
+	int testcases=sc.nextInt();
+	int targetAmount;
+	int j,sum,flag;
+	for(int i=1;i<=testcases;i++)
+	{
+		sum=0;
+		flag=0;
+		System.out.println("Enter Target Amount of " + i + "case");  
+		targetAmount=sc.nextInt();
+		for(j=0;j<trans.length;j++)
+		{
+			sum=sum+trans[j];
+			if(sum>=targetAmount)
+			{
+				System.out.println("Target complete on "+ ++j + "day" );
+				flag=1;
+				break;
+			}
+		}
+		if(flag==0)
+		System.out.println("Target not fulfilled" );
+	}
+	}
+}
+
